0e2f2e5a34e13bd9a5bc45cb7981cf929079a069
Devlin Duldulao <webmasterdevlin@gmail.com>
2025-10-23 21:42:55 +0200

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vercel from "vite-plugin-vercel";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), vercel()],
+  plugins: [react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
